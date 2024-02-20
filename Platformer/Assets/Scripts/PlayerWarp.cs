f3b48b2d8708a767aa300eec1c0e0bb19f711b81
Wreckitshoot <90283922+Wreckitshoot@users.noreply.github.com>
2024-02-20 11:18:29 -0500

    Warp Script

---
+++ Platformer/Assets/Scripts/PlayerWarp.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWarp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private bool in_UG = false;
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private int UG_distance = 50;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (!in_UG)
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y - UG_distance);
+                in_UG = true;
+            }
+            else if (in_UG)
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y + UG_distance);
+                in_UG = false;
+            }
+        }
+    }
+}
